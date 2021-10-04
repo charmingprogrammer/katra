bfb9f90b245063312665eed63e9be4628f2b317d
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-10-04 20:33:12 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,19 @@
+function facebookPage() {
+ document.getElementById('page01').style.display = "block";
+ document.getElementById('page03').style.display = "none";
+ 
+}
+function usefullsite() {
+ document.getElementById('page03').style.display = "block";
+ document.getElementById('page01').style.display = "none";
+ 
+}
+function AddPostContainerOpen() {
+  document.getElementById('AddPostContainer').style.transform = 'scale(1)';
+}
+
+function AddPostContainerClose() {
+  document.getElementById('AddPostContainer').style.transform = 'scale(0)';
+}
+
+
